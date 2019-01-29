888f14c597d074873eb3838b98dc811d9d295dff
Andre Muess <andre.muess@adindex.de>
2019-01-29 11:09:37 +0100

    fixed syntax error

--- lib/util/validator.rb
+++ lib/util/validator.rb
@@ -12,8 +12,12 @@ module Magentwo
             raise ArgumentError, "#{attribute} must be set" if model.send(attribute).nil?
           end
         else
-          raise ArgumentError "unknown model type, expected Child of Magentwo::Base or Hash"
+          raise ArgumentError, "unknown model type, expected Child of Magentwo::Base or Hash"
         end
+      end
+
+      def one_of value, *valid_values
+        raise ArgumentError, "value #{value} invalid, expected one of #{valid_values}" unless valid_values.include?value
       end
     end
   end
