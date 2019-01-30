42e0457de0f9dbda327c743af63a3eba779672ad
Andre Muess <andre.muess@adindex.de>
2019-01-30 11:35:08 +0100

    added generate method to salesRule model to generate coupons

--- lib/model/sales_rule.rb
+++ lib/model/sales_rule.rb
@@ -3,6 +3,11 @@ module Magentwo
     Attributes = %i(rule_id name store_labels description website_ids customer_group_ids uses_per_customer is_active condition action_condition stop_rules_processing is_advanced sort_order simple_action discount_amount discount_step apply_to_shipping times_used is_rss coupon_type use_auto_generation uses_per_coupon simple_free_shipping)
     Attributes.each do |attr| attr_accessor attr end
 
+    def generate quantity:1, length:16, format:(:alpha), delimiter:"-", delimiter_at_every:4
+      Magentwo::Coupon.generate self.rule_id, quantity:quantity, length:length, format:format, delimiter:delimiter, delimiter_at_every:delimiter_at_every
+    end
+
+
     class << self
       def get_path
         "#{base_path}/search"
