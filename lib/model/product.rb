a7a79107a25127b0397b2d5c5aa86ec36ae41a7e
Andre Muess <andre.muess@adindex.de>
2019-01-30 11:42:21 +0100

    added method types to product class

--- lib/model/product.rb
+++ lib/model/product.rb
@@ -2,5 +2,11 @@ module Magentwo
   class Product < Base
     Attributes = %i(id sku name attribute_set_id price status visibility type_id created_at updated_at extension_attributes product_links options media_gallery_entries tier_prices custom_attributes)
     Attributes.each do |attr| attr_accessor attr end
+
+    class << self
+      def types
+        Magentwo::Base.get nil, path:"#{base_path}/types"
+      end
+    end
   end
 end
