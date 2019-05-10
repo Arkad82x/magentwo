3a3f0a31bf11d27ee228b4d68d318a254f84f9cd
darkrubyist <darkrubyist@users.noreply.github.com>
2019-05-11 01:56:52 +0300

    Update Category.rb
    Hi,
    
    I wanted to import all categories into my PostgreSQL DB and I notice the Category.rb is still not finished.
    Because I couldn't make it work in a similar way the other models work, I went through Swagger API docs and found that the correct base path is /categories/list if you want to return an array of categories.
    
    However, because I was dealing with 10k Categories, I was getting the following error: Net::ReadTimeout (Net::ReadTimeout)
    So, I built a custom get_categories method. Please let me know if everything is correct and make sense.
    
    Best,

--- lib/model/category.rb
+++ lib/model/category.rb
@@ -1,9 +1,45 @@
+require 'ostruct'
+
 module Magentwo
   class Category < Base
-    #TODO implement
+    Attributes = %i(id parent_id name is_active position level product_count children_data)
+    Attributes.each do |attr| attr_accessor attr end
+
     class << self
+      
+      # Getting all categories and nested categories from /categories
+      def get_categories
+        @categories = []
+        result = Magentwo::Base.adapter.call(:get, 'categories', '')
+        object = JSON.parse(result.to_json, object_class: OpenStruct)
+        parse_categories(object)
+        return @categories
+      end
+
+      # Loop recursively through all categories, build an OStruct for each category
+      # Append the category object to @categories
+      def parse_categories(categories)
+        @categories << build_category(categories)
+        categories.children_data.each do |category|
+          parse_categories(category)
+        end
+      end
+      
+      def build_category(object)
+        category = OpenStruct.new
+        Magentwo::Category::Attributes.each do |attr| 
+          category[attr] = object[attr] unless attr == :children_data
+        end
+        return category
+      end  
+
+
+      # Based on the Mangeto Swagger Documentation the correct base path is "categories/list"
+      # However, I working with more than 10k categories and I'm receiving a Net::ReadTimeout (Net::ReadTimeout) error
+      # and the page limit filter doesn't seem to be working so I built the Get Cateogires method above.
+      
       def base_path
-        "categories"
+        "categories/list"
       end
     end
   end
